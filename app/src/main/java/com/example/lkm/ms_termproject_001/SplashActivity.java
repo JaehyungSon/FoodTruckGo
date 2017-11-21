cfbeeafc3754436185c8edb9f0699cda53a3a381
hopefuler <hopefuler@naver.com>
2017-11-22 02:35:17 +0900

    메뉴

--- app/src/main/java/com/example/lkm/ms_termproject_001/SplashActivity.java
+++ app/src/main/java/com/example/lkm/ms_termproject_001/SplashActivity.java
@@ -43,7 +43,7 @@ public class SplashActivity extends AppCompatActivity {
 
                 requestMe();
             }
-        }, 3000);
+        }, 3500);
     }
 
     class SplashView extends View {
