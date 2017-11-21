761c96304e3bb29ffccda52e8e46db63c1719665
JaehyungSon <add3217@naver.com>
2017-11-21 22:38:50 +0900

    수정중

--- app/src/main/java/com/example/lkm/ms_termproject_001/FoodtrcukRegistActivity.java
+++ app/src/main/java/com/example/lkm/ms_termproject_001/FoodtrcukRegistActivity.java
@@ -1,9 +1,12 @@
 package com.example.lkm.ms_termproject_001;
 
+import android.*;
+import android.Manifest;
 import android.content.Context;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -22,14 +25,12 @@ public class FoodtrcukRegistActivity extends AppCompatActivity {
     FirebaseDatabase fd;    //데이터베이스
     DatabaseReference Ref;
     TextView tv;
-    ToggleButton tb;
+  //  ToggleButton tb;
 
+    double longitude=0;  //경도
+    double latitude=0;   //위도
+    double altitude=0;   //고도
 
-
-
-    LocationManager locationManager;    //경도 위도
-    double longitudeBest, latitudeBest;
-    double longitudeGPS, latitudeGPS;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,42 +40,30 @@ public class FoodtrcukRegistActivity extends AppCompatActivity {
         FoodtruckSimpleExplain = (EditText)findViewById(R.id.FoodtruckSimpleExplain);
         FoodtruckExplain= (EditText)findViewById(R.id.FoodtruckExplain);
 
-
+        ActivityCompat.requestPermissions(FoodtrcukRegistActivity.this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
 
 
 
         tv = (TextView) findViewById(R.id.textView2);
-        tv.setText("위치정보 미수신중");
 
-        tb = (ToggleButton)findViewById(R.id.toggle1);
-
-        // LocationManager 객체를 얻어온다
         final LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
 
-        tb.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try{
-                    if(tb.isChecked()){
-                        tv.setText("수신중..");
-                        // GPS 제공자의 정보가 바뀌면 콜백하도록 리스너 등록하기~!!!
-                        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, // 등록할 위치제공자
-                                100, // 통지사이의 최소 시간간격 (miliSecond)
-                                1, // 통지사이의 최소 변경거리 (m)
-                                mLocationListener);
-                        lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, // 등록할 위치제공자
-                                100, // 통지사이의 최소 시간간격 (miliSecond)
-                                1, // 통지사이의 최소 변경거리 (m)
-                                mLocationListener);
-                    }else{
-                        tv.setText("위치정보 미수신중");
-                        lm.removeUpdates(mLocationListener);  //  미수신할때는 반드시 자원해체를 해주어야 한다.
-                    }
-                }catch(SecurityException ex){
-                }
-            }
-        });
+        try{
+
+            lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, // 등록할 위치제공자
+                    100, // 통지사이의 최소 시간간격 (miliSecond)
+                    1, // 통지사이의 최소 변경거리 (m)
+                    mLocationListener);
+            lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, // 등록할 위치제공자
+                    100, // 통지사이의 최소 시간간격 (miliSecond)
+                    1, // 통지사이의 최소 변경거리 (m)
+                    mLocationListener);
+
+    //                lm.removeUpdates(mLocationListener);  //  미수신할때는 반드시 자원해체를 해주어야 한다.
+
+        }catch(SecurityException ex){
+        }
 
 
     }
@@ -94,6 +83,9 @@ public class FoodtrcukRegistActivity extends AppCompatActivity {
         data.put("simpleExplain",FoodtruckSimpleExplain.getText().toString());
         data.put("explain",FoodtruckExplain.getText().toString());
         data.put("openFlag","0");
+        data.put("경도",longitude+"");
+        data.put("위도",latitude+"");
+        data.put("고도",altitude+"");
 
         HashMap<String,Object> child = new HashMap<String,Object>();
         child.put(key,data);
@@ -114,16 +106,17 @@ public class FoodtrcukRegistActivity extends AppCompatActivity {
             //값은 Location 형태로 리턴되며 좌표 출력 방법은 다음과 같다.
 
             Log.d("test", "onLocationChanged, location:" + location);
-            double longitude = location.getLongitude(); //경도
-            double latitude = location.getLatitude();   //위도
-            double altitude = location.getAltitude();   //고도
+            longitude = location.getLongitude(); //경도
+            latitude = location.getLatitude();   //위도
+            altitude = location.getAltitude();   //고도
             float accuracy = location.getAccuracy();    //정확도
             String provider = location.getProvider();   //위치제공자
+
+
             //Gps 위치제공자에 의한 위치변화. 오차범위가 좁다.
             //Network 위치제공자에 의한 위치변화
             //Network 위치는 Gps에 비해 정확도가 많이 떨어진다.
-            tv.setText("위치정보 : " + provider + "\n위도 : " + longitude + "\n경도 : " + latitude
-                    + "\n고도 : " + altitude + "\n정확도 : "  + accuracy);
+            tv.setText("위도 : " + longitude + "\n경도 : " + latitude);
         }
         public void onProviderDisabled(String provider) {
             // Disabled시
