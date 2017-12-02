e3eafa289d5f9e52cd0d083b3e5db0b5fbf15f35
JaehyungSon <add3217@naver.com>
2017-12-02 21:47:40 +0900

    즐겨찾기 페이지 구현 완료

--- app/src/main/java/com/example/lkm/ms_termproject_001/BookmarkActivity.java
+++ app/src/main/java/com/example/lkm/ms_termproject_001/BookmarkActivity.java
@@ -1,17 +1,28 @@
 package com.example.lkm.ms_termproject_001;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.kakao.network.ErrorResult;
 import com.kakao.usermgmt.UserManagement;
 import com.kakao.usermgmt.callback.MeResponseCallback;
@@ -22,6 +33,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 
 public class BookmarkActivity extends AppCompatActivity {
     String name = "ERROR";    //카카오와 연동하기위해
@@ -29,6 +41,10 @@ public class BookmarkActivity extends AppCompatActivity {
     String profilePhotoURL = "";
     Bitmap bitmap;
     private SimpleSideDrawer mSlidingMenu;
+    private ListView mListView;
+    ArrayList<Integer> bookmarkArraylist = new ArrayList<>();
+    ArrayList<Integer> bookmarkViewlist = new ArrayList<>();
+    BookmarkMyAdapter mMyAdapter = new BookmarkMyAdapter();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +55,47 @@ public class BookmarkActivity extends AppCompatActivity {
         mSlidingMenu.setLeftBehindContentView(R.layout.activiry_left_menu);
 
         requestMe();  //카카오 정보 load
+
+        mListView=(ListView)findViewById(R.id.bookmarkListView);
+        temp();
+        localBookmarkGet(); //저장된 리스트 불러오기
+        dataSetting();  //리스트뷰에 뿌려주기
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
+
+                final CharSequence[] items = {"자세히 보기", "즐겨찾기 삭제 ㅜㅜ"};
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(BookmarkActivity.this);     // 여기서 this는 Activity의 this
+
+                builder.setTitle(mMyAdapter.getItem(position).getName())        // 제목 설정
+                        .setItems(items, new DialogInterface.OnClickListener(){    // 목록 클릭시 설정
+                            public void onClick(DialogInterface dialog, int index){
+                                switch (index){
+                                    case 0:
+                                        Toast.makeText(BookmarkActivity.this, "자세히보기로 이동", Toast.LENGTH_SHORT).show();
+                                        break;
+                                    case 1:
+                                        localBookmarkRemove(position);
+                                        bookmarkArraylist.clear();
+                                        bookmarkViewlist.clear();
+                                        mMyAdapter.removeAll();
+                                        localBookmarkGet(); //저장된 리스트 불러오기
+                                        dataSetting();  //리스트뷰에 뿌려주기
+                                        break;
+                                }
+                            }
+                        });
+
+                AlertDialog dialog = builder.create();    // 알림창 객체 생성
+                dialog.show();    // 알림창 띄우기
+
+
+            }
+        });
+
+
     }
 
     // ------- 왼쪽 메뉴바 관련 코드 start ------- //
@@ -200,4 +257,111 @@ public class BookmarkActivity extends AppCompatActivity {
         });
     }
     // ------- 카카오 유저정보 가져오기 end ------- //
+
+
+
+    //파일 저장 코드 (즐겨찾기 목록)
+    private void savePreferences(int index,int value){
+        SharedPreferences pref = getSharedPreferences("pref", MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putString(String.valueOf(index), String.valueOf(value));
+        editor.commit();
+    }
+
+    //파일 저장 코드 (즐겨찾기 목록)
+    private void temp(){
+        SharedPreferences pref = getSharedPreferences("pref", MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putString(String.valueOf(0), String.valueOf(566779173));
+        editor.putString(String.valueOf(1), String.valueOf(566781065));
+        editor.commit();
+    }
+    //로컬에 저장되어있는 파일을 가져옴
+    private void localBookmarkGet(){
+        int i=0;
+        while(true){
+            if(getPreferences(String.valueOf(i)).equals("")){
+                break;
+            }else{
+                bookmarkArraylist.add(Integer.parseInt(getPreferences(String.valueOf(i))));
+            }
+            i++;
+        }
+
+    }
+    //파일에서 값 가져오기 (즐겨찾기 목록)
+    private String getPreferences(String key){
+        SharedPreferences pref = getSharedPreferences("pref", MODE_PRIVATE);
+        return pref.getString(key, "");
+    }
+
+    private void localBookmarkRemove(int index){
+        savePreferences(bookmarkArraylist.indexOf(bookmarkViewlist.get(index)),0);
+    }
+
+
+    // ------- 리스트 뷰 start ------- //
+
+    String name_2="";
+    String simpleExplain="기본글";
+    String photo="";
+    private void dataSetting(){
+        FirebaseDatabase fd = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = fd.getReference().child("FoodTrucks");
+
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Log.d("asdfasdf", "Value is: " + dataSnapshot);
+
+                final Bitmap[] tempBitmap = new Bitmap[1];
+                //String photo="";
+                for(DataSnapshot child : dataSnapshot.getChildren()){
+                    Log.e("key",child.getKey());
+
+                    if(bookmarkArraylist.indexOf(Integer.parseInt(child.getKey()))==-1){
+
+                        continue;
+                    }
+                    bookmarkViewlist.add(Integer.parseInt(child.getKey()));
+
+                    for(DataSnapshot childchild : child.getChildren()){
+                        if(childchild.getKey().equals("name")){
+                            //      Log.e("we do",childchild.getValue().toString());
+                            name_2 = childchild.getValue().toString();
+
+                        }
+                        if(childchild.getKey().equals("simpleExplain")){
+                            simpleExplain = childchild.getValue().toString();
+                        }
+                        if(childchild.getKey().equals("1")){
+                            photo = childchild.getValue().toString();
+
+                        }
+
+                    }
+                    mMyAdapter.addItem(photo,name_2,simpleExplain,"100m");
+                    mMyAdapter.notifyDataSetChanged();
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("asdfasdf", "Failed to read value.", error.toException());
+            }
+        });
+
+
+        mListView.setAdapter(mMyAdapter);
+
+
+
+        /* 리스트뷰에 어댑터 등록 */
+
+
+    }
 }
---
+++ app/src/main/java/com/example/lkm/ms_termproject_001/BookmarkMyItem.java
@@ -0,0 +1,42 @@
+package com.example.lkm.ms_termproject_001;
+
+public class BookmarkMyItem {
+
+    private String icon;
+    private String name;
+    private String contents;
+    private String distance;
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+
+    public String getdistance() {
+        return distance;
+    }
+
+    public void setdistance(String distance) {
+        this.distance = distance;
+    }
+
+}
